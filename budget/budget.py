5708b3b66e6a6b9b78ebf4ae2418c582d85d0ec7
Martin Hagstrom <marhag87@gmail.com>
2018-02-02 10:58:19 +0100

    More SQLite testing

--- budget/budget.py
+++ budget/budget.py
@@ -7,6 +7,7 @@ from pyyamlconfig import (
     load_config,
     PyYAMLConfigError,
 )
+from pprint import pprint
 
 
 class Budget:
@@ -20,9 +21,12 @@ class Budget:
         try:
             self.config = load_config(config)
         except PyYAMLConfigError:
-            pass
-        self.config = {**self.config, **default_config}
-        self.database = pyodbc.connect(self.config.get('connectionstring'))
+            self.config = {}
+        self.config = {**default_config, **self.config}
+        self.database = pyodbc.connect(
+            self.config.get('connectionstring'),
+            autocommit=True,
+        )
         self.setup_database()
 
     def setup_database(self):
@@ -60,39 +64,186 @@ class Budget:
         cursor.execute(
             '''
             CREATE TABLE
-                transaction (
+                event (
                     id integer primary key autoincrement,
                     title_id integer NOT NULL,
                     date date NOT NULL,
                     amount integer NOT NULL,
-                    balance integer NOT NULL
+                    balance integer NOT NULL,
+                    FOREIGN KEY(title_id) REFERENCES title(id)
                 )
             '''
         )
-        self.database.commit()
 
-    def get_category(self):
+    def get_categories(self):
         cursor = self.database.cursor()
         cursor.execute('SELECT * FROM category')
         return cursor.fetchall()
 
-    def get_transactions(self, year, month):
-        """Example function for printing a set of transactions"""
+    def get_titles(self):
         cursor = self.database.cursor()
-        cursor.execute(f"SELECT * FROM transactions_for_month({year}, {month});")
-        return cursor.fetchone()
+        cursor.execute('SELECT * FROM title')
+        return cursor.fetchall()
 
-    def insert_transactions(self, transactionfile='transactions.txt'):
+    def get_events(self, year, month):
+        """Example function for printing a set of events"""
+        cursor = self.database.cursor()
+        cursor.execute(
+            f'''
+            SELECT
+                title.name as title,
+                date,
+                amount,
+                balance,
+                category.name
+            FROM
+                event
+            JOIN
+                title
+            ON
+                title.id = event.title_id
+            JOIN
+                category_map
+            ON
+                title.id = category_map.title_id
+            JOIN
+                category
+            ON
+                category_map.category_id = category.id
+            WHERE
+                date BETWEEN '{year}-{month:02}-01' AND '{year}-{month:02}-31'
+            '''
+        )
+        return cursor.fetchall()
+
+    def create_category(self, category):
+        cursor = self.database.cursor()
+        cursor.execute(
+            f'''
+            SELECT
+                id
+            FROM
+                category
+            WHERE
+                name = "{category}"
+            '''
+        )
+        category_id = cursor.fetchone()
+        if category_id is None:
+            cursor.execute(
+                f'''
+                INSERT INTO
+                    category (
+                        name
+                    )
+                VALUES (
+                    "{category}"
+                )
+                '''
+            )
+            cursor.execute(
+                f'''
+                SELECT
+                    id
+                FROM
+                    category
+                WHERE
+                    name = "{category}"
+                '''
+            )
+            category_id = cursor.fetchone()
+        return category_id
+
+    def set_category(self, title_id, category_id):
+        cursor = self.database.cursor()
+        cursor.execute(
+            f'''
+            INSERT INTO
+                category_map (
+                    title_id,
+                    category_id
+                )
+            VALUES (
+                {title_id},
+                {category_id}
+            )
+            '''
+        )
+
+    def insert_events(self, eventfile='events.txt'):
         """Insert all the transactions found in the file provided"""
         cursor = self.database.cursor()
-        with open(transactionfile) as file:
+        with open(eventfile) as file:
             lines = file.readlines()
             while lines:
                 title = lines.pop(0).strip()
-                cursor.execute(f"SELECT * FROM insert_title('{title}');")
-                (title_id,) = cursor.fetchone()
-                return title_id
+                date = lines.pop(0).strip()
+                amount = lines.pop(0).strip()
+                amount = int(amount.replace(',', '').replace(' ', ''))
+                balance = lines.pop(0).strip()
+                balance = int(balance.replace(',', '').replace(' ', ''))
+                cursor.execute(
+                    f'''
+                    SELECT
+                        id
+                    FROM
+                        title
+                    WHERE
+                        name = "{title}"
+                    '''
+                )
+                try:
+                    (title_id,) = cursor.fetchone()
+                except TypeError:
+                    title_id = None
+                if title_id is None:
+                    cursor.execute(
+                        f'''
+                        INSERT INTO
+                            title (
+                                name
+                            )
+                        VALUES (
+                            "{title}"
+                        );
+                        '''
+                    )
+                    cursor.execute(
+                        f'''
+                        SELECT
+                            id
+                        FROM
+                            title
+                        WHERE
+                            name = "{title}"
+                        '''
+                    )
+                    (title_id,) = cursor.fetchone()
+
+                cursor.execute(
+                    f'''
+                    INSERT INTO
+                        event (
+                            title_id,
+                            date,
+                            amount,
+                            balance
+                        )
+                    VALUES (
+                        {title_id},
+                        "{date}",
+                        {amount},
+                        {balance}
+                    )
+                    '''
+                )
 
 
 if __name__ == '__main__':
-    print(Budget().get_category())
+    budget = Budget()
+    budget.insert_events()
+    budget.create_category('Groceries')
+    budget.create_category('Electronics')
+    budget.set_category(1, 1)
+    pprint(budget.get_events(2018, 1))
+    #pprint(budget.get_titles())
